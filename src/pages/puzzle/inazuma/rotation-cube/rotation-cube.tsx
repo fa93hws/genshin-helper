83ebf2597fb6c372879f6ba09a0e3d04160f6e27
Eric Wang <wjun0912@gmail.com>
2021-08-29 22:50:59 +1000

    chore: rename control to quantity config (#13)

--- src/pages/puzzle/inazuma/rotation-cube/rotation-cube.tsx
+++ src/pages/puzzle/inazuma/rotation-cube/rotation-cube.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
-import { createControl } from './control/control';
+import { createQuantityConfig } from './quantity-config/quantity-config';
 
 export function createRotationCubePage() {
-  const { Component } = createControl({ nCubes: 3, minCubes: 3, maxCubes: 6 });
+  const { Component } = createQuantityConfig({
+    nCubes: 3,
+    minCubes: 3,
+    maxCubes: 6,
+  });
   return React.memo(() => <Component />);
 }
